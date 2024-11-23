eb3e9a520b2dc7914018eb6540eafc561e449486
Kailash <iamkailashofficial@gmail.com>
2024-11-24 00:36:07 +0545

    provider version updated

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.74.2"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
   # access_key = var.AWS_ACCESS_KEY_ID
