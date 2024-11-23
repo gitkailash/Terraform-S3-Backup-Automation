fd6a89c3fa6386dc57a678d5bcee19bf154347a5
Kailash <iamkailashofficial@gmail.com>
2024-11-23 22:04:14 +0545

    github action value added

--- variables.tf
+++ variables.tf
@@ -11,7 +11,24 @@ variable "backup_retention_days" {
 }
 
 variable "region" {
-  description = "The AWS region to deploy resources"
-  type        = string
+  description = "The AWS region to deploy resources in"
   default     = "us-east-1"
 }
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "The AWS access key ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "The AWS secret access key"
+  type        = string
+  sensitive   = true
+}
+
+variable "AWS_PROFILE" {
+  description = "The AWS CLI profile name"
+  default     = "default"
+}
+
