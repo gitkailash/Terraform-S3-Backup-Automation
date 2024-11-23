e95ecdd637a53d9a63ebd58c51333538beb26c5b
Kailash <iamkailashofficial@gmail.com>
2024-11-23 22:26:51 +0545

    update

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region     = var.region
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
-  profile    = var.AWS_PROFILE
+  # access_key = var.AWS_ACCESS_KEY_ID
+  # secret_key = var.AWS_SECRET_ACCESS_KEY
+  # profile    = var.AWS_PROFILE
 }
 
 
